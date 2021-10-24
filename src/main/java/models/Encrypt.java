bdb447458ddad51e421d2b609cc734dba1be5353
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-24 17:46:45 +0300

    This commit adds the encrypt class and creates a constructor for the class

---
+++ src/main/java/models/Encrypt.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Encrypt {
+    private String text;
+    private int shift;
+
+    public Encrypt(String text, int shift){
+        this.text = text;
+        this.shift = shift;
+    }
+
+
+}
